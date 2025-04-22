08250d8e23ba22b4e92a0b2a4fb93a2cca77db5f
mbisch11 <mbisch11@gmail.com>
2025-04-22 16:02:38 -0400

    Fixed the add assignment activity

--- app/src/main/java/com/example/assigneder/AddAssignment.java
+++ app/src/main/java/com/example/assigneder/AddAssignment.java
@@ -1,5 +1,6 @@
 package com.example.assigneder;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,14 +11,31 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class AddAssignment extends AppCompatActivity {
+public class AddAssignment extends AppCompatActivity implements View.OnClickListener{
+
+    public static final String SHARED_PREF_NAME = "ASSIGNEDER";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_assignment);
 
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREF_NAME, MODE_PRIVATE);
+
         Button addButton = findViewById(R.id.button_add_assignment);
         addButton.setOnClickListener((View.OnClickListener) this);
+
+        Button cancelButton = findViewById(R.id.button_cancel);
+        cancelButton.setOnClickListener((View.OnClickListener) this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+
+        if(id == R.id.button_cancel){
+            finish();
+        }
     }
 }
