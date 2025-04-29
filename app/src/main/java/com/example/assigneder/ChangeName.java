40ce936ef72f22c534d8b3f3161209c9b8be3301
mbisch11 <mbisch11@gmail.com>
2025-04-28 21:13:13 -0400

    Added functionality to change name

--- app/src/main/java/com/example/assigneder/ChangeName.java
+++ app/src/main/java/com/example/assigneder/ChangeName.java
@@ -1,5 +1,6 @@
 package com.example.assigneder;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -7,14 +8,20 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.snackbar.Snackbar;
+
 public class ChangeName extends AppCompatActivity implements View.OnClickListener {
+
+    private SharedPreferences sharedPreferences;
 
     public static final String SHARED_PREF_NAME = "ASSIGNEDER";
     public static final String NAME_KEY = "STUDENT_NAME";
@@ -24,14 +31,44 @@ public class ChangeName extends AppCompatActivity implements View.OnClickListene
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_name);
 
-        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREF_NAME, MODE_PRIVATE);
+        this.sharedPreferences = getSharedPreferences(SHARED_PREF_NAME, MODE_PRIVATE);
 
         Button saveButton = findViewById(R.id.save_button);
+        saveButton.setOnClickListener(this);
+    }
+
+    public String getInputFromEditText(int id) {
+        EditText v = findViewById(id);
+        return v.getText().toString();
+    }
+
+    public void changeName(String name){
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(NAME_KEY, name);
+        editor.apply();
     }
 
     @Override
     public void onClick(View v) {
-
+        String name = getInputFromEditText(R.id.change_name_field);
+        if(name.isBlank()){
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle(R.string.error_header);
+            builder.setMessage(R.string.alert_box_empty_name);
+            builder.setNegativeButton(R.string.alert_box_ok_button, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                }
+            });
+            builder.show();
+        }else {
+            changeName(name);
+            Button button = findViewById(R.id.save_button);
+            Snackbar.make(button,
+                    "Changed name to " + name + "!",
+                    Snackbar.LENGTH_LONG).show();
+        }
     }
 
     @Override
